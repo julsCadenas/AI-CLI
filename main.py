1e2fdd46aa15beac222f2fae3bbd4b7a68af70ac
julsCadenas <jscadenas12@gmail.com>
2025-01-22 00:16:12 +0800

    separated main functions into a menu.py file for better organization

--- main.py
+++ main.py
@@ -1,43 +1,24 @@
 #!/usr/bin/env python3
 import typer
 import os
+from menu import Menu
 from dotenv import load_dotenv
-from utils.meta import Meta_AI
-from rich import print
-import sys
 
 load_dotenv()
+
+url = os.getenv("llama_url")
+token = os.getenv("llama_token")
+
 app = typer.Typer()
+choice = Menu(url, token)
 
 @app.command()
 def meta():
-    url = os.getenv("llama_url")
-    token = os.getenv("llama_token")
-    
-    if not url or not token:
-        print("[bold red]Error: Missing URL or token from environment variables[/bold red]")
-        sys.exit(1)
-
-    meta = Meta_AI(url, token)
-    
-    print("[bold green]Welcome to the MetaAI chatbot! Type 'exit' to end the conversation.[/bold green]")
-    
-    while True:
-        print("[bold blue]You:[/bold blue]", end=" ")
-        query = input()
-        
-        if query.lower() == "exit":
-            print("[bold green]Goodbye! Have a great day![/bold green]")
-            break
-        
-        response = meta.llm_query(query)
-        
-        meta.get_response(query, response)
+    choice.meta()
 
 @app.command()
 def exit():
-    print("[bold cyan]Goodbye![/bold cyan]")
-    sys.exit(0)
+    choice.exit()
 
 if __name__ == "__main__":
     app()
---
+++ menu.py
@@ -0,0 +1,38 @@
+import sys
+from rich import print
+from utils.meta import Meta_AI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Menu:
+    def __init__(self, url, token):
+        self.url = url
+        self.token = token
+    
+    def exit(self):  
+        print("[bold cyan]Goodbye![/bold cyan]")
+        sys.exit(0)
+
+    def meta(self):  
+        url = self.url
+        token = self.token
+        
+        if not url or not token:
+            print("[bold red]Error: Missing URL or token from environment variables[/bold red]")
+            sys.exit(1)
+
+        meta = Meta_AI(url, token)
+        
+        print("[bold green]Welcome to the MetaAI chatbot! Type 'exit' to end the conversation.[/bold green]")
+        
+        while True:
+            print("[bold blue]You:[/bold blue]", end=" ")
+            query = input()
+            
+            if query.lower() == "exit":
+                print("[bold green]Goodbye! Have a great day![/bold green]")
+                break
+            
+            response = meta.llm_query(query)
+            meta.get_response(query, response)
